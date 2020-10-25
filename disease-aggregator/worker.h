5ad66b91e6c148bc07aad319f5912c9056a6faef
Dimitrios Dedoussis <ddedoussis@gmail.com>
2020-10-25 23:54:46 +0000

    Rephrase vars

--- disease-aggregator/worker.h
+++ disease-aggregator/worker.h
@@ -16,11 +16,11 @@ public:
   void set_queue(Fifo queue);
   pid_t get_pid() const;
   void set_pid(pid_t pid);
-  inline bool operator==(const WorkerSettings &other) const {
-    return get_pid() == other.get_pid();
+  inline bool operator==(const WorkerSettings &rhs) const {
+    return get_pid() == rhs.get_pid();
   };
-  inline bool operator<(const WorkerSettings &other) const {
-    return get_pid() < other.get_pid();
+  inline bool operator<(const WorkerSettings &rhs) const {
+    return get_pid() < rhs.get_pid();
   };
 };
 struct WorkerData {
